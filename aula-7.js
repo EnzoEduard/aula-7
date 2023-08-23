8e994b09fe461247716d6efdaac84c23c90b40ab
enzo <142436206+EnzoEduard@users.noreply.github.com>
2023-08-23 17:13:33 -0300

    Create aula-7.js

---
+++ aula-7.js
@@ -0,0 +1,88 @@
+/*
+let a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+*/
+/*
+let minhaIdade = 16;
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1;
+console.log("Depois", minhaIdade);
+*/
+
+/*
+let a = 3;
+let b = 4;
+let c = 2;
+let d = 5;
+let e = -1;
+let f = 234;
+ console.log(a + b);
+console.log(a * d / c);
+console.log((d - b) * (e)); 
+console.log(f % d);
+*/ 
+/*
+let a = 3;
+let b = 5;
+ console.log(a === b);
+ console.log(a !== b);
+ console.log(a >= b);
+ console.log(a <= b);
+*/
+ /*
+let a = 5;
+let b = 3;
+let c = 0;
+ 
+console.log(a === b || a !== c);
+console.log(a === b && a !== c);
+console.log("---- separador ----");
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/
+/*
+let a = true;
+let b = false;
+let c = true;
+
+console.log(a && b);
+console.log(b && c);
+console.log(a && c);
+console.log(a && b && c);
+
+console.log(a || b);
+console.log(b || c);
+console.log(a || c);
+console.log(a || b || c);
+*/
+/*
+let a = true;
+let b = false;
+
+console.log(a);
+console.log(!a);
+console.log(b);
+console.log(!b);
+*/
+/*
+let nome = "enzo";
+let idade = 16;
+console.log("nome:","enzo");
+console.log("idade:", 16)
+console.log("É maior de idade?",true && false);
+let n = 2007;
+let f = 2050;
+console.log("idade em 2050:", f - n);
+*/
+
